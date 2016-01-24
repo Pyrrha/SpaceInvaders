b1533827293a9e22bf18698d9b74bb5db76ad11a
Pyrrha <pyrrha@geeksdelacave.com>
2016-01-24 23:49:40 +0100

    Edit

--- compile.sh
+++ compile.sh
@@ -8,24 +8,19 @@
 
 #### EN-TETES ####
 # Liaison des en-tête de la librairie SFML pour le compilateur.
-
 g++ -c *.cpp *.h -std=c++11 -ISFML-2.3.2/include
-
-
 
 #### BIBLIOTHEQUES ####
 # Indication au chargeur de bibliothèques d'où elles se trouvent
-
 export LD_LIBRARY_PATH=SFML-2.3.2/lib
 
-
-
 #### MODULES ####
-# Liaisiob des modules system et audio avec l'exécutable
+# Liaision des modules system et audio avec l'exécutable
+g++ *.o -o SpaceInvaders -LSFML-2.3.2/lib -lsfml-audio -lsfml-system
 
-g++ *.o -o sfml-app -LSFML-2.3.2/lib -lsfml-audio -lsfml-system
-
-
+#### PERMISSIONS ####
+# Pour rendre le fichier exécutable
+chmod +x SpaceInvaders
 
 #### LANCEMENT ####
-./sfml-app
+./SpaceInvaders
